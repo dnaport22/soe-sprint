6cc8d46bd502f28f2705eb05fa41112ba72c28b0
skorekm <skorekmarcin@gmail.com>
2017-05-12 17:11:53 +0100

    loging out the user

--- www/js/controller/dash/settingsctrl.js
+++ www/js/controller/dash/settingsctrl.js
@@ -1,20 +1,30 @@
-soe.controller('settingsctrl', function ($scope, $ionicTabsDelegate, $ionicHistory, $state) {
-  $ionicTabsDelegate.showBar(false);
+soe.controller('settingsctrl', function ($scope, $ionicTabsDelegate, $ionicHistory, $state, $localStorage) {
+  	$ionicTabsDelegate.showBar(false);
   
 	$scope.notificationHandler = function (event) {
 		//make call to the backend
 		console.log(event.target.checked);
-	}
+	};
 
 	$scope.openAbout = function () {
 		$state.go('tab.about');
-	}
+	};
 
-	$scope.openPrivacyPolicy = function() {
+	$scope.openPrivacyPolicy = function () {
 		$state.go('tab.privacypolicy');
-	}
+	};
+
 	$scope.openTermsAndConditions = function () {
 		$state.go('tab.termsandconditions');
+	};
+
+	$scope.logoutUser = function () {
+		$localStorage.soe_user_status = 0;
+	    $localStorage.soe_user_username = null;
+	    $localStorage.soe_user_email = null;
+	    $localStorage.soe_user_token = null;
+	    $localStorage.expiry = null;
+		$state.go('tab.dash');
 	};
 
 });
