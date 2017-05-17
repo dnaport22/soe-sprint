fc21064d16204d90ebea9f44075e7ed27300e2a6
skorekm <skorekmarcin@gmail.com>
2017-05-17 14:23:30 +0100

    fixed login issue with history

--- www/js/controller/mainctrl.js
+++ www/js/controller/mainctrl.js
@@ -13,6 +13,21 @@ soe.controller('mainctrl', function ($scope, $localStorage, $state, $ionicHistor
     $state.go('tab.feedback');
   }
 
+  $scope.openSettings = function () {
+    $state.go('tab.settings');
+  };
+
+   // Setting up local variables for app navigation logic
+  $scope.$storage = $localStorage.$default({
+    soe_user_uid: null,
+    soe_user_username: null,
+    soe_user_token: null,
+    soe_user_email: null,
+    soe_user_status: 0,
+    soe_expiry: 0,
+    soe_app_launch_activity: 0
+  });
+
     // Navigate use to login page if not logged in
   if ($localStorage.soe_user_status == 0) {
     $ionicHistory.nextViewOptions({
